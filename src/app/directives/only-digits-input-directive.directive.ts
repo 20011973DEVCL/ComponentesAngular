449aac98cffa2b5a3d4e8c417e2697efbac20070
Danilo Alarcon Lopez <danilo.alarcon.lopez@icloud.com>
2024-04-24 18:37:04 -0400

    first commit

--- src/app/directives/only-digits-input-directive.directive.ts
+++ src/app/directives/only-digits-input-directive.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appOnlyDigitsInputDirective]',
@@ -6,6 +6,15 @@ import { Directive } from '@angular/core';
 })
 export class OnlyDigitsInputDirectiveDirective {
 
-  constructor() { }
+  constructor(private el:ElementRef) { }
+
+ @HostListener('input', ['$input']) onInputChange(event: Event):void {
+  const input = event.target as HTMLInputElement;
+  const initialValue = input.value;
+
+  const cleanedValue = initialValue.replace(/[])
+
+  /Users/developer/Pictures/Photos Library.photoslibrary/resources/derivatives/F/FBA3FA0E-0CB9-4F59-BC3B-46CF8953670A_1_105_c.jpeg
+ }
 
 }
